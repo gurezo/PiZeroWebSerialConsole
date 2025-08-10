f54f7a15afac225c8898a102aaab998c7278dd4b
akihiko.kigure <akihiko.kigure@gmail.com>
2025-08-10 14:13:26 +0900

    refactor: split interface

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,27 +1,4 @@
-/// <reference types="@types/w3c-web-serial" />
-
-export interface FileInfo {
-  name: string;
-  size: number;
-  isDirectory: boolean;
-}
-
-export interface WiFiInfo {
-  address: string;
-  essid: string;
-  spec: string;
-  quality: string;
-  frequency: string;
-  channel: string;
-}
-
-export interface SerialPortWrapper {
-  port: SerialPort;
-  reader: ReadableStreamDefaultReader<Uint8Array>;
-  writer: WritableStreamDefaultWriter<Uint8Array>;
-}
-
-export interface SourcePath {
-  fileName: string;
-  dir: string;
-}
+export * from './file-info';
+export * from './serial-port-wrapper';
+export * from './source-path';
+export * from './wifi-info';
